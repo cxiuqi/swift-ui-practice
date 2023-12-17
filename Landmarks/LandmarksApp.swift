6805426088d3dcee4607a57c9bf84866001fea1e
cxiuqi <xiugoood@gmail.com>
2023-12-17 22:55:39 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Xiu Qi Chang on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
