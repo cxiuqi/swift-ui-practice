f1433ebd1893b457dd41558e7a0e49e52bbcfc80
cxiuqi <xiugoood@gmail.com>
2023-12-25 22:20:30 +0800

    Initial Commit

---
+++ Landmarks/View/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Xiu Qi Chang on 25/12/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
